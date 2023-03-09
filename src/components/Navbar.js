81382a20c913c18448b3cd8c22c244e6952f1687
MrTompkinsGamow <mhbrandenburgh@gmail.com>
2023-03-08 22:16:42 -0500

    passing props

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
-export default function Navbar(handlePageChange) {
+export default function Navbar(props) {
     return (
-        <nav className=''>
-            <div onClick={() => handlePageChange('about')}>Matt Brandenburgh</div>
+        <nav className='flex-row flex'>
+            <div onClick={() => props.pikachu('about')}>Matt Brandenburgh</div>
             <div>
-            <div onClick={() => handlePageChange('blog')} classname='border-5px p-4'>Blog</div>
-            <div onClick={() => handlePageChange('projects')} classname='border-5px p-4'>Projects</div>
-            <div onClick={() => handlePageChange('contact')} classname='border-5px p-4'>Contact</div>
+            <div onClick={() => props.pikachu('blog')} classname='border-5px p-4'>Blog</div>
+            <div onClick={() => props.pikachu('projects')} classname='border-5px p-4'>Projects</div>
+            <div onClick={() => props.pikachu('contact')} classname='border-5px p-4'>Contact</div>
             </div>
             
         </nav>
